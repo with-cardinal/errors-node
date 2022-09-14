0a61680fe5c6b2ae6e1e3a0564096de37bc6d6e1
Alan Johnson <nalanj@fastmail.com>
2022-09-14 15:32:21 -0400

    Got uncaught exceptions in browser working

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -8,15 +8,6 @@ test("homepage has Playwright in title and get started link linking to the intro
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Uncaught Exception");
 
-  // // create a locator
-  // const getStarted = page.locator('text=Get Started');
-
-  // // Expect an attribute "to be strictly equal" to the value.
-  // await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // // Click the get started link.
-  // await getStarted.click();
-
-  // // Expects the URL to contain intro.
-  // await expect(page).toHaveURL(/.*intro/);
+  const p = page.locator("p");
+  await expect(p).toHaveText("Something broke");
 });
--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,18 @@
 import { _init, defaultOptions, Options } from "./config";
 
-export function init(options: Options = defaultOptions) {
+type ErrorsWindow = Window & { Errors: { init: typeof init } };
+declare let window: ErrorsWindow;
+
+function init(options: Options = defaultOptions) {
   _init(options);
+
+  window.onerror = (msg, url, line, column, err) => {
+    options.errorCallback(err);
+  };
+
+  window.onunhandledrejection = (evt) => {
+    options.errorCallback(evt.reason);
+  };
 }
+
+window.Errors = { init };
