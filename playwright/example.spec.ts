da52b0f747b0fa519cba8db2adbaec0bf8792895
Alan Johnson <nalanj@fastmail.com>
2022-09-14 15:49:00 -0400

    Catch unhandled rejections

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,13 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test("homepage has Playwright in title and get started link linking to the intro page", async ({
-  page,
-}) => {
+test("uncaught exception", async ({ page }) => {
   await page.goto("./uncaught-exception.html");
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Uncaught Exception");
 
   const p = page.locator("p");
   await expect(p).toHaveText("Something broke");
+});
+
+test("unhandled rejection", async ({ page }) => {
+  await page.goto("./unhandled-rejection.html");
+  await expect(page).toHaveTitle("Unhandled Rejection");
+
+  const p = page.locator("p");
+  await expect(p).toHaveText("Unhandled rejection");
 });
