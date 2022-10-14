624f3af8c83092c6e633bb0b927faf15a6c2eec1
Alan Johnson <nalanj@fastmail.com>
2022-10-14 16:23:06 -0400

    Make all options optional for init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
-export type SendableError = {
+function defaultErrorCallback(e: unknown) {
+  const sendable = errorToSendable(e);
+  console.log(sendable);
+}
+
+export type Options = {
+  secret: string;
+  errorCallback: typeof defaultErrorCallback;
+};
+
+export const defaultOptions = {
+  secret: process.env.ERRORS_SECRET,
+  errorCallback: defaultErrorCallback,
+};
+
+let _options: Options;
+export function init(options: Partial<Options> = defaultOptions) {
+  Object.assign(_options, options, defaultOptions);
+
+  process.on("unhandledRejection", send);
+  process.on("uncaughtException", send);
+}
+
+export function send(e: unknown) {
+  _options.errorCallback(e);
+}
+
+type SendableError = {
   message: string;
   at: Date;
   stack?: string;
@@ -19,28 +46,4 @@ export function errorToSendable(e: unknown): SendableError {
       at,
     };
   }
-}
-
-function defaultErrorCallback(e: unknown) {
-  const sendable = errorToSendable(e);
-  console.log(sendable);
-}
-
-export type Options = {
-  errorCallback: typeof defaultErrorCallback;
-};
-
-export const defaultOptions = {
-  errorCallback: defaultErrorCallback,
-};
-
-let _options: Options;
-export function init(options: Options = defaultOptions) {
-  _options = options;
-  process.on("unhandledRejection", send);
-  process.on("uncaughtException", send);
-}
-
-export function send(e: unknown) {
-  _options.errorCallback(e);
 }
