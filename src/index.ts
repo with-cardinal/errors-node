1074e563356c36a9baa2f41428cc59a561d170eb
Alan Johnson <nalanj@fastmail.com>
2022-10-14 16:17:01 -0400

    Reorganize everything

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
-import { _init, send, defaultOptions, Options } from "./config";
+export type SendableError = {
+  message: string;
+  at: Date;
+  stack?: string;
+};
 
+export function errorToSendable(e: unknown): SendableError {
+  const at = new Date();
+
+  if (e instanceof Error) {
+    return {
+      message: e.message,
+      at,
+      stack: e.stack,
+    };
+  } else {
+    return {
+      message: String(e),
+      at,
+    };
+  }
+}
+
+function defaultErrorCallback(e: unknown) {
+  const sendable = errorToSendable(e);
+  console.log(sendable);
+}
+
+export type Options = {
+  errorCallback: typeof defaultErrorCallback;
+};
+
+export const defaultOptions = {
+  errorCallback: defaultErrorCallback,
+};
+
+let _options: Options;
 export function init(options: Options = defaultOptions) {
-  _init(options);
-
+  _options = options;
   process.on("unhandledRejection", send);
   process.on("uncaughtException", send);
+}
+
+export function send(e: unknown) {
+  _options.errorCallback(e);
 }
