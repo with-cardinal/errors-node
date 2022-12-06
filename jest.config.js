d5305f39fec5de304f63c8759aa324350691e699
Alan Johnson <nalanj@fastmail.com>
2022-12-06 14:51:22 -0500

    Switch to ts-jest for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  transform: {
-    "^.+\\.(t|j)sx?$": ["@swc/jest"],
-  },
-  testPathIgnorePatterns: ["/node_modules/", "/dist/", "playwright"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  resetMocks: true,
 };
